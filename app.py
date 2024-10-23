cc96bb27197b0c286742aeb0f95c12ab6be6b782
Abishek Upadhyay <98577878+Google-Ka-Dinosaur@users.noreply.github.com>
2024-10-23 19:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pickle
+import sklearn
+import numpy as np
 import pandas as pd
 pipe1=pickle.load(open('./1st_pipe.pkl','rb'))
 pipe2=pickle.load(open('./2nd_pipe.pkl','rb'))
